c9bf5be3b3f2fd3d60a8c8c9af4c7efd051fea27
varunsingh <varsingh@seas.upenn.edu>
2025-12-31 23:06:33 +0530

    Datasets

--- dataScripts/cali.py
+++ dataScripts/cali.py
@@ -7,39 +7,37 @@ except Exception as e:
     ee.Authenticate()
     ee.Initialize(project='hale-life-482914-r0')
 
-
-## Plumas county -> California (Dixie fire)
+# Plumas County / Dixie Fire Region
 roi_ca = ee.Geometry.Rectangle([-121.20, 39.80, -120.50, 40.50])
 
+# Input Image (Now with 10 bands - excellent choice)
 image = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED') \
     .filterBounds(roi_ca) \
     .filterDate('2024-06-01', '2024-09-01') \
     .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 5)) \
     .median() \
-    .select(['B4', 'B3', 'B2', 'B8']) 
-    # Red, Green, Blue, NIR
+    .select(['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A', 'B11', 'B12']) 
 
-# Get the Label (Hansen Global Forest Change)
-# We want to find pixels that were lost specifically during the fire (2021)
+# Label (Forest Loss)
 hansen = ee.Image('UMD/hansen/global_forest_change_2024_v1_12')
-# 'lossyear' is the year of loss (0-24). 21 = 2021.
-# We create a binary mask: 1 = Burned in 2021, 0 = Other
-loss_mask = hansen.select('lossyear').eq(21).rename('burned_label')
 
-final_stack = image.addBands(loss_mask)
+loss_mask = hansen.select('lossyear').eq(21).rename('burned_label').toFloat()
 
-# Export to Google Drive
+# Create Stack
+final_stack = image.addBands(loss_mask).toFloat()
+
+# Export
 task = ee.batch.Export.image.toDrive(
-    image=image.addBands(loss_mask),
-    description='California_Dixie_Fire_2024',
-    folder='Nature_Restoration_Project',
-    scale=10, # 10 meter resolution
+    image=final_stack,  # Use the stack variable here
+    description='California_Dixie_Fire_2024_Full',
+    folder='Nature_Restoration_California',
+    scale=10, 
     region=roi_ca,
     fileFormat='GeoTIFF',
     maxPixels=1e10,
-    fileDimensions=256,
+    fileDimensions=[256, 256],
     skipEmptyTiles=True
 )
 
 task.start()
-print("Downloading.....")
+print("Downloading... Check https://code.earthengine.google.com/tasks")
--- dataScripts/louisana.py
+++ dataScripts/louisana.py
@@ -18,26 +18,26 @@ image = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED') \
     .filterDate('2024-03-01', '2024-05-01') \
     .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10)) \
     .median() \
-    .select(['B4', 'B3', 'B2', 'B8'])
+    .select(['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A', 'B11', 'B12']) 
 
 # Get the Label (JRC Global Surface Water)
 # 'occurrence' is 0-100% water frequency over 38 years.
 # High occurrence = Water (1), Low = Land (0).
 water_hist = ee.Image('JRC/GSW1_4/GlobalSurfaceWater')
-water_label = water_hist.select('occurrence').gt(80).rename('water_label')
+water_label = water_hist.select('occurrence').gt(80).rename('water_label').toFloat()
 
-final_stack = image.addBands(water_label)
+final_stack = image.addBands(water_label).toFloat()
 
 # Export to Google Drive
 task = ee.batch.Export.image.toDrive(
     image=final_stack,
     description='Louisiana_Wetland_2024',
-    folder='Nature_Restoration_Project',
+    folder='Nature_Restoration_Louisiana',
     scale=10,
     region=roi_la,
     fileFormat='GeoTIFF',
     maxPixels=1e10,
-    fileDimensions=256,
+    fileDimensions=[256, 256],
     skipEmptyTiles=True
 )
 
